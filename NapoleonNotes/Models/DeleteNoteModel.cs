1901f8aab6677479c6058ac6ceba5a267ad9a5be
Sergey Korenyugin <korenb23@mail.ru>
2019-03-05 22:58:17 +0500

    api models

---
+++ NapoleonNotes/Models/DeleteNoteModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NapoleonNotes.Models
+{
+    public class DeleteNoteModel
+    {
+        [Required]
+        public Guid Id { get; set; }
+    }
+}
---
+++ NapoleonNotes/Models/NoteModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NapoleonNotes.Models
+{
+    public class NoteModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTimeOffset DateCreate { get; set; }
+        public DateTimeOffset DateUpdate { get; set; }
+    }
+}
