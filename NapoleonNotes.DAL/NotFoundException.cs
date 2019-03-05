bddfe61c2bb5fb6e7958f8fd0d80030cb637af27
Sergey Korenyugin <korenb23@mail.ru>
2019-03-05 22:50:38 +0500

    добавил хранилище заметок

---
+++ NapoleonNotes.DAL/NotFoundException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NapoleonNotes.DAL
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException()
+            :base("The entity not found")
+        {
+
+        }
+
+        public NotFoundException SetId(Guid guid)
+        {
+            Data.Add("id", guid);
+            return this;
+        }
+    }
+}
--- NapoleonNotes.DAL/NoteRepository.cs
+++ NapoleonNotes.DAL/NoteRepository.cs
@@ -1,9 +1,100 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NapoleonNotes.DAL
 {
-    public class NoteRepository
+    /// <summary>
+    /// Хранилище заметок (in memory)
+    /// </summary>
+    public interface INoteRepository
     {
+        /// <summary>
+        /// Cоздать заметку
+        /// </summary>
+        /// <param name="note">инстанс</param>
+        /// <returns></returns>
+        Note Create(Note note);
+        /// <summary>
+        /// Получить заметку
+        /// </summary>
+        /// <param name="guid">идентификатор</param>
+        /// <returns></returns>
+        Note Get(Guid guid);
+        /// <summary>
+        /// Получить заметки
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Note> Get();
+        /// <summary>
+        /// Редактировать заметку
+        /// </summary>
+        /// <param name="note"></param>
+        /// <exception cref="NotFoundException"></exception>
+        void Edit(Note note);
+        /// <summary>
+        /// Удалить заметку
+        /// </summary>
+        /// <param name="guid">идентификатор</param>
+        void Remove(Guid guid);
+        /// <summary>
+        /// Удалить заметку
+        /// </summary>
+        /// <param name="note">заметка</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        void Remove(Note note);
+    }
+    public class NoteRepository : INoteRepository
+    {
+        private List<Note> _notes;
 
+        public NoteRepository()
+        {
+            _notes = new List<Note>();
+        }
+
+        public Note Create(Note note)
+        {
+            note.Id = Guid.NewGuid();
+            _notes.Add(note);
+            return note;
+        }
+
+        public void Edit(Note note)
+        {
+            int index = _notes.FindIndex(e => e.Id == note.Id);
+            if (index < 0)
+                throw new NotFoundException()
+                    .SetId(note.Id);
+
+            _notes[index] = note;
+        }
+
+        public Note Get(Guid guid)
+        {
+            return _notes.Find(e => e.Id == guid);
+        }
+
+        public IEnumerable<Note> Get()
+        {
+            return _notes;
+        }
+
+        public void Remove(Guid guid)
+        {
+            var entity = _notes.Find(e => e.Id == guid);
+            if (entity == null)
+                throw new NotFoundException()
+                    .SetId(guid);
+
+            Remove(entity);
+        }
+
+        public void Remove(Note note)
+        {
+            if (note == null)
+                throw new ArgumentNullException(nameof(note));
+
+            _notes.Remove(note);
+        }
     }
 }
